37b67a5551369124b51aa223ecf6f377bae4db76
jovanaoukar <jovanaoukar@hotmail.com>
2024-04-22 14:45:39 +0200

    Add Passenger model

--- reservations/gestion/models.py
+++ reservations/gestion/models.py
@@ -3,4 +3,9 @@ from django.db import models
 class Station(models.Model):
     name = models.CharField(max_length=200)
 
+class Passenger(models.Model):
+    surname = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    
 
