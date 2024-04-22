0674b13c5bad8ab78bfea366df6fd105f89f178a
jovanaoukar <jovanaoukar@hotmail.com>
2024-04-22 15:40:59 +0200

    Add gestion App to reservations/settings.py

--- reservations/reservations/settings.py
+++ reservations/reservations/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gestion.apps.GestionConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
