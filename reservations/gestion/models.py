431f3d964239f16e5af92757cb1a4df002bfc2b1
jovanaoukar <jovanaoukar@hotmail.com>
2024-04-22 14:52:25 +0200

    Add Client model

--- reservations/gestion/models.py
+++ reservations/gestion/models.py
@@ -8,4 +8,11 @@ class Passenger(models.Model):
     first_name = models.CharField(max_length=100)
     date_of_birth = models.DateField()
     
+class Client(models.Model):
+    surname = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    email = models.EmailField()
+    address = models.CharField(max_length=300)
+    phone = models.CharField(max_length=15)
+    
 
