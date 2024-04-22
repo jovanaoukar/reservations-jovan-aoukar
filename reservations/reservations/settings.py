f73ee5b1c7e0beb2d9f1492060bb74cd80e34172
jovanaoukar <jovanaoukar@hotmail.com>
2024-04-22 15:45:12 +0200

    Fix gestion App bug in reservations/settings.py

--- reservations/reservations/settings.py
+++ reservations/reservations/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'gestion.apps.GestionConfig'
+    'gestion.apps.GestionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
