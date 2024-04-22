7f318daeea4b7b26fa4107905fda5c8274b001c1
jovanaoukar <jovanaoukar@hotmail.com>
2024-04-22 15:51:23 +0200

    Fix errors in migrations in gestion/models.py

--- reservations/gestion/models.py
+++ reservations/gestion/models.py
@@ -16,14 +16,14 @@ class Client(models.Model):
     phone = models.CharField(max_length=15)
     
 class Route(models.Model):
-    departure_station = models.ForeignKey(Station, on_delete=models.CASCADE)
+    departure_station = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='departure_station')
     departure_time = models.DateTimeField()
-    arrival_station = models.ForeignKey(Station, on_delete=models.CASCADE)
+    arrival_station = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='arrival_station')
     arrival_time = models.DateTimeField()
 
 class Reservation(models.Model):
     reservation_date = models.DateTimeField()
-    reservation_number = models.AutoField(unique=True)
+    reservation_number = models.CharField(max_length=10, unique=True)
     seat_number = models.IntegerField()
     route = models.ForeignKey(Route, on_delete=models.CASCADE)
     passenger = models.ForeignKey(Passenger, on_delete=models.CASCADE)
