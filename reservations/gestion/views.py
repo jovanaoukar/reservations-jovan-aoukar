72b221daa38e963d57c34b7f33f2259bd77859de
jovanaoukar <jovanaoukar@hotmail.com>
2024-04-22 16:35:29 +0200

    Start using templates and static for gestion App

--- reservations/gestion/views.py
+++ reservations/gestion/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Future home page, under construction...")
+    return render(request, "gestion/index.html")
