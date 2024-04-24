8f19f091e4d4fae50b900a07c8d9ebfdcec35120
jovanaoukar <jovanaoukar@hotmail.com>
2024-04-24 23:51:07 +0200

    Add routes view

--- reservations/gestion/urls.py
+++ reservations/gestion/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("routes", views.routes, name="routes"),
 ]
--- reservations/gestion/views.py
+++ reservations/gestion/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Route
 
 def index(request):
     return render(request, "gestion/index.html")
+
+def routes(request):
+    routes_list = Route.objects.order_by("departure_time")
+    return render(request, "gestion/routes.html", {"routes_list": routes_list})
