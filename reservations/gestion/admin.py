1a501e021ddeaebb4d3e1e8880af0ff0ec228be2
jovanaoukar <jovanaoukar@hotmail.com>
2024-04-26 00:52:44 +0200

    Fix: Register Passenger model to admin console

--- reservations/gestion/admin.py
+++ reservations/gestion/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Station, Route, Client, Reservation
+from .models import Station, Route, Client, Passenger, Reservation
 
 admin.site.register(Station)
 admin.site.register(Route)
 admin.site.register(Client)
+admin.site.register(Passenger)
 admin.site.register(Reservation)
 
