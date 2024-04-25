a6dc6ee4a230be4b069d8e38619c9a6a6ef933b3
jovanaoukar <jovanaoukar@hotmail.com>
2024-04-25 22:53:42 +0200

    Add auth views and /accounts/login.html page and add / path

--- reservations/reservations/urls.py
+++ reservations/reservations/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from gestion import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("manage/", include("gestion.urls"))
+    path("manage/", include("gestion.urls")),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', views.index, name='home')
 ]
