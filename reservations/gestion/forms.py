9ea70f3d66f768fee5f9d279d1825f0bdae7b1c7
jovanaoukar <jovanaoukar@hotmail.com>
2024-04-29 14:09:23 +0200

    Fix migrations problem

--- reservations/gestion/forms.py
+++ reservations/gestion/forms.py
@@ -3,7 +3,7 @@ from .models import Station
 
 
 class SearchRouteForm(forms.Form):
-    station_choices = [("","Select...")] + [(station.name, station.name) for station in Station.objects.all().order_by()]
+    station_choices = [("","Select...")]
     
     departure_station_name = forms.CharField(
         label='Departure Station',
@@ -18,4 +18,8 @@ class SearchRouteForm(forms.Form):
         max_length=200,
         required=False
         )
+    
+    def __init__(self, *args, **kwargs):
+        super(SearchRouteForm, self).__init__(*args, **kwargs)
+        self.fields['station_choices'].choices = [("","Select...")] + [(station.name, station.name) for station in Station.objects.all().order_by()]
     
