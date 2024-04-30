96304e98cd619cb122a26410b58606f078d36cc2
jovanaoukar <jovanaoukar@hotmail.com>
2024-05-01 01:24:08 +0200

    Bug Fix: Change SearchRouteForm fields to forms.ModelChoiceField in gestion/forms.py

--- reservations/gestion/forms.py
+++ reservations/gestion/forms.py
@@ -3,25 +3,16 @@ from .models import Station
 
 
 class SearchRouteForm(forms.Form):
-    station_choices = [("","Select...")]
-    
-    departure_station_name = forms.CharField(
-        label='Departure Station',
-        widget=forms.Select(choices=station_choices),
-        max_length=200,
-        required=False
-        )
-    
-    arrival_station_name = forms.CharField(
-        label='Arrival Station',
-        widget=forms.Select(choices=station_choices),
-        max_length=200,
-        required=False
-        )
-    
-    def __init__(self, *args, **kwargs):
-        super(SearchRouteForm, self).__init__(*args, **kwargs)
-        station_choices = [("","Select...")] + [(station.name, station.name) for station in Station.objects.all().order_by()]
-        self.fields['departure_station_name'].choices = station_choices
-        self.fields['arrival_station_name'].choices = station_choices
+    departure_station_name = forms.ModelChoiceField(
+        Station.objects.all().order_by("name"),
+        empty_label = "Select...",
+        to_field_name = "name",
+        required = False
+    )
+    arrival_station_name = forms.ModelChoiceField(
+        Station.objects.all().order_by("name"),
+        empty_label = "Select...",
+        to_field_name = "name",
+        required = False
+    )
     
